c59e1fe0889a600cd15ee265222369622e54ca3b
Sverre Holmbakken <111376082+SverreHolmbakken@users.noreply.github.com>
2022-11-30 12:14:12 +0100

    First commit

---
+++ js/modules/cart-copy.js
@@ -0,0 +1,7 @@
+export default function CartCopy() {
+	let items = GetItemsLocally();
+
+	const cartAddButton = document.querySelector('.add-to-cart-button');
+	const cartItemsContainer = document.querySelector('.cart__items');
+	const cart
+}
