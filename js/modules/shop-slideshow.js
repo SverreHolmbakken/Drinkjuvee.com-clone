cbb9dfbbe144712227650bb46d9aecb977c94d01
Sverre Holmbakken <111376082+SverreHolmbakken@users.noreply.github.com>
2022-11-30 12:11:32 +0100

    Implemented different indexes for different "slides"

--- js/modules/shop-slideshow.js
+++ js/modules/shop-slideshow.js
@@ -1,20 +1,67 @@
 export default function ShopSlideshow() {
-	let currentIndex
+	let currentIndex = 0;
+	let index
+	let count = [
+		{
+			count: 1
+		},
+		{
+			count: 1
+		},
+		{
+			count: 1
+		},
+		{
+			count: 1
+		},
+	]
+	
 
 	// Query selectors
 	const slideshow = document.querySelector('.shop-slideshow');
 	const slideshowSlides = document.querySelectorAll('.shop__slide');
-	const buttons = document.querySelectorAll('.shop__item-button');
+	const productButtons = document.querySelectorAll('.shop__item-button');
+	const inputNumber = document.querySelectorAll('.shop__add-to-cart-number');
+	const subtractButton = document.querySelectorAll('.shop__subtract');
+	const additionButton = document.querySelectorAll('.shop__add');
 
 	// Event listeners
-	buttons.forEach(button => {
+	productButtons.forEach(button => {
 		button.addEventListener('click', handleButtonSwitchSlide)
 	})
+
+	subtractButton.forEach(button => {
+		button.addEventListener('click', handleDecreaseNumber)
+	})
+	
+	additionButton.forEach(button => {
+		button.addEventListener('click', handleIncreaseNumber)
+	})
+	
+	// subtractButton.addEventListener('click', handleDecreaseNumber);
+	// additionButton.addEventListener('click', handleIncreaseNumber);
 
 	// Event handlers
 	function handleButtonSwitchSlide(event) {
 		currentIndex = event.currentTarget.dataset.index
 		updateSlideshowHTML()
+	}
+		if (count > 1) {
+			count--;
+			inputNumber[currentIndex].innerHTML = count;
+	}
+	function handleDecreaseNumber(event) {
+		if (count[currentIndex].count >= 1) {
+			count[currentIndex].count -=1
+			inputNumber[currentIndex].innerHTML = count[currentIndex].count;
+		}
+	}
+
+	function handleIncreaseNumber(event) {
+		if (count[currentIndex].count >= 1) {
+			count[currentIndex].count +=1
+			inputNumber[currentIndex].innerHTML = count[currentIndex].count;
+		}
 	}
 	
 
@@ -25,4 +72,5 @@ export default function ShopSlideshow() {
 		}
 		slideshowSlides[currentIndex].classList.add('shop__slide--active')
 	}
+
 }
