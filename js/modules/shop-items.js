df1844b30db876caa1dae600edc6efd82f1142a9
Sverre Holmbakken <111376082+SverreHolmbakken@users.noreply.github.com>
2022-12-08 15:18:20 +0100

    Added array of shop items

---
+++ js/modules/shop-items.js
@@ -0,0 +1,29 @@
+// All of the items info
+const shopItems = [
+	{
+		name: 'Kiwi Strawberry',
+		image: 'assets/images/cart/Juvee_Tray_Mockup_Kiwi_2048x.webp',
+		description: '12 pack',
+		price: 36,
+	},
+	{
+		name: 'Tropical Crush',
+		image: 'assets/images/cart/Juvee_Tray_Mockup_Tropical_2000x.webp',
+		description: '12 pack',
+		price: 36,
+	},
+	{
+		name: 'Watermelon Lime',
+		image: 'assets/images/cart/Juvee_Tray_Mockup_Watermelon_2000x.webp',
+		description: '12 pack',
+		price: 36,
+	},
+	{
+		name: 'Variety Pack',
+		image: 'assets/images/cart/juvee_variety_pack_2000x.webp',
+		description: '12 pack',
+		price: 36,
+	}
+]
+
+export {shopItems};
